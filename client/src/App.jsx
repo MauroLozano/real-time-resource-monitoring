43921fe88696d9b0e467931c3a0c504c3685e654
Mauro Lozano <mauroloz2006@gmail.com>
2025-12-19 19:58:18 -0300

    feat: create request to get the static data and show it on the browser

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
-
+import { useState, useEffect } from "react";
+import StaticSection from "./components/StaticSection";
 function App() {
   return (
-    <h1>Real Time Resource Monitoring</h1>
+    <>
+      <h1>Real Time Resource Monitoring</h1>
+      <StaticSection></StaticSection>
+    </>
   )
 }
 
---
+++ client/src/components/StaticSection.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+const serverUrl = 'http://localhost:3000'
+
+import { useState, useEffect } from "react";
+
+function StaticSection(){
+    const [staticData, setStaticData] = useState(null) 
+    useEffect(()=>{
+    fetch(`${serverUrl}/staticData`)
+        .then((res)=>{
+        if(!res.ok) console.error('Server error') 
+        return res.json()
+        })
+        .then(data =>{ 
+        setStaticData(data)
+        })
+        .catch(error => console.error(error))
+    }, [])
+
+    return (
+        <section>
+            {!staticData? (<p>Cargando</p>) : (
+                <>
+                    <h2>Static Data</h2>
+                    <h3>CPU</h3>
+                    <p>Brand: {staticData.cpu.brand}</p>
+                    <p>Manufacturer: {staticData.cpu.manufacturer}</p>
+                    <p>Amount of cores: {staticData.cpu.cores}</p>
+                    <h3>Memory</h3>
+                    <p>Total: {staticData.memory.total}</p>
+                    <h3>Storage</h3>
+                    {
+                        staticData.storage.map((disk, index) =>(
+                            <div key={index}>
+                                <p>Name: {disk.name}</p>
+                                <p>Type: {disk.type}</p>
+                                <p>Total size: {disk.size}</p>
+                            </div>
+                        ))
+                    }
+                </>
+            )}
+        </section>
+    )
+}
+export default StaticSection
