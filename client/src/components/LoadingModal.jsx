7f792b8cf7f5eda5fb7ac5b671bd5fa1da432c61
Mauro Lozano <mauroloz2006@gmail.com>
2025-12-24 21:28:42 -0300

    feat: LoadingModal will have border radius and a different background color

--- client/src/components/LoadingModal.jsx
+++ client/src/components/LoadingModal.jsx
@@ -3,7 +3,7 @@ import { BarLoader } from 'react-spinners';
 
 export default function LoadingModal({color}){
     return(
-        <div style={{width: "100%", height: "100%", display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <div style={{width: "100%", height: "100%", display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: "rgba(0, 0, 0, 0.10)", borderRadius: '1rem'}}>
             <BarLoader color={color} width={'80%'}></BarLoader>
         </div>
     )
