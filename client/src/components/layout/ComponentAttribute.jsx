7a3158bde962d073d1bb0418b6f38d3df38492bc
Mauro Lozano <mauroloz2006@gmail.com>
2026-02-04 23:10:52 -0300

    feat(errors): show 'unknown' on static data if it couldn't be retrieved

--- client/src/components/layout/ComponentAttribute.jsx
+++ client/src/components/layout/ComponentAttribute.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import styles from './ComponentAttribute.module.css'
 export default function ComponentAttribute({label, value}){
+    const hasError = value === null || value === undefined ||value === ''
     return(
-        <>
-            <div className={styles.attributeContainer}>
-                <span className={styles.label}>{label}</span>
-                <span className={styles.value}>{value}</span>
-            </div>
-        </>
+        <div className={styles.attributeContainer}>
+            <span className={styles.label}>{label}</span>
+            <span className={`${styles.value} ${hasError ? styles.unknown : ''}`}>{hasError? 'Unknown':value}</span>
+        </div>
     )
 }
