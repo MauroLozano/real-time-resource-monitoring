1f65ecfd16462903e004979f32095546aa2ec28c
MauroLozano <mauroloz2006@gmail.com>
2026-03-22 22:12:37 -0300

    feat(cpuTemp): create logic to get the cpu temp in linux systems

--- server/src/services/cpuTempService.ts
+++ server/src/services/cpuTempService.ts
@@ -1,25 +1,54 @@
 import type { CoreTemp, CpuTempServiceResponse } from '../types/temperature.js';
 import { formattedFloat } from '../utils/formatter.js';
 import { getWmiTemperature } from '../utils/cpuTempService.js';
-import { cpu } from 'systeminformation';
+import si from "systeminformation";
+import { platform } from 'node:os';
 
 export const getCpuTempData = async (): Promise<CpuTempServiceResponse> => {
-  let wmiData = await getWmiTemperature();
-  const coresTemp: CoreTemp[] | null = wmiData.coresTemp;
-  let thermalHeadroom: number | null = 0;
+  let cpuTempData: CpuTempServiceResponse = { coresTemp: null, cpuTemp: null, thermalHeadroom: null };
+  if (platform() == "linux") {
+    let rawSiData = await si.cpuTemperature();
+
+    let cpuTemp: number | null = rawSiData.main > 0 ? rawSiData.main : null;
+
+    let coresTemp: CoreTemp[] | null = rawSiData.cores.length > 0 ? rawSiData.cores.map((entry, i) => {
+      const coreNumber = i;
+      return {
+        coreNumber: coreNumber,
+        value: entry,
+      }
+    }) : null;
+
+    cpuTempData = {
+      cpuTemp: cpuTemp,
+      coresTemp: coresTemp,
+      thermalHeadroom: getThermalHeadroom(coresTemp, cpuTemp),
+    };
+
+  } else if (platform() == "win32") {
+    let wmiData = await getWmiTemperature();
+    const coresTemp: CoreTemp[] | null = wmiData.coresTemp;
+    let cpuTemp: number | null = wmiData.packageTemp ? wmiData.packageTemp : null;
+    const thermalHeadroom = getThermalHeadroom(coresTemp, cpuTemp);
+
+    cpuTempData = {
+      cpuTemp: cpuTemp,
+      coresTemp: coresTemp && coresTemp?.length > 0 ? coresTemp : null,
+      thermalHeadroom: thermalHeadroom,
+    };
+  }
+  return cpuTempData;
+};
+
+function getThermalHeadroom(coresTemp: CoreTemp[] | null, cpuTemp: number | null): number | null {
+  let thermalHeadroom: number | null = null;
   if (coresTemp && coresTemp?.length > 0) {
     const coresTempValues: number[] = coresTemp.map((entry) => {
       return entry.value;
     });
     thermalHeadroom = formattedFloat(100 - Math.max(...coresTempValues));
-  } else if (wmiData.packageTemp != null) {
-    thermalHeadroom = formattedFloat(100 - wmiData.packageTemp);
+  } else if (cpuTemp != null) {
+    thermalHeadroom = formattedFloat(100 - cpuTemp);
   }
-
-  const cpuTempData: CpuTempServiceResponse = {
-    cpuTemp: wmiData.packageTemp,
-    coresTemp: coresTemp && coresTemp?.length > 0 ? coresTemp : null,
-    thermalHeadroom: thermalHeadroom,
-  };
-  return cpuTempData;
-};
+  return thermalHeadroom
+}
